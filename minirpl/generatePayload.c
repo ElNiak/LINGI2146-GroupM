dd2ab8bbee8344fc788d895aa73877e6b43014d5
ElNiak <christophe.crochet@student.uclouvain.be>
2019-05-07 12:10:56 +0200

    generatePayload(cliendID,topicID)

--- minirpl/generatePayload.c
+++ minirpl/generatePayload.c
@@ -5,9 +5,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <assert.h>
 #include <math.h> //gcc -lm
 
-char * generateData(char * ci) {
+char * generateData(char * ci, char * topic) {
+    assert(strlen(topic) == 1);
     double data = (-50.0) + (rand()/(RAND_MAX/(90.0 - (-50.0))));
     double roundedData = round(data * 100) /100;
     int i = 0;
@@ -17,12 +19,12 @@ char * generateData(char * ci) {
     snprintf(outData, i+6, "%f", roundedData);
     char * res = (char *) malloc(sizeof(ci) + 1 + sizeof(outData)+1);
     strcat(res,ci);
-    strcat(res,"1");
+    strcat(res,topic);
     strcat(res,outData);
     return res;
 }
 
 int main( int argc, const char* argv[]) {
     for(int i = 0 ; i < 10; i ++)
-        printf("%d : %s\n",i,generateData("1"));
+        printf("%d : %s\n",i,generateData("1","2"));
 }
